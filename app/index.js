c90779f28b58303d3f399010015b026e3a2a8034
fow5040 <fow5040@yahoo.com>
2020-06-07 02:11:38 -0400

    First pass of replicating 'Fix'

--- app/index.js
+++ app/index.js
@@ -1,3 +1,41 @@
-console.log('Hello world!');
+import clock from "clock";
+import document from "document";
 
+// Update the clock every second
+clock.granularity = "seconds";
 
+let seconds_group = document.getElementById('seconds_group');
+let minutes_group = document.getElementById('minutes_group');
+let hours_group = document.getElementById('hours_group');
+let seconds = document.getElementById("seconds");
+
+let getMinSecX = (val,radius) => radius * Math.cos(Math.PI * val / 30 + .98*Math.PI);
+let getMinSecY = (val,radius) => radius * Math.sin(Math.PI * val / 30 + .98*Math.PI);
+
+let getHourX = (hour,radius) => radius * Math.cos(Math.PI * hour / 6 + .98*Math.PI);
+let getHourY = (hour,radius) => radius * Math.sin(Math.PI * hour / 6 + .98*Math.PI);
+
+function updateClock(evt) {
+  //for testing
+  //let today = new Date(2020, 3, 3, 2, 15, 0);
+  let today = evt.date;
+  let secs = today.getSeconds();
+  let mins = today.getMinutes();
+  let hours = today.getHours() % 12;
+
+  for (let ind = 0; ind < 12; ind += 1){
+    //seconds_group.children[ind].x = getMinSecX(ind*5 - secs, 50);
+    //seconds_group.children[ind].y = getMinSecY(ind*5 - secs, 50);
+
+    minutes_group.children[ind].x = getMinSecX(ind*5 - mins, 100);
+    minutes_group.children[ind].y = getMinSecY(ind*5 - mins, 100);
+    
+    hours_group.children[ind].x = getHourX(ind - hours - mins/60, 170);
+    hours_group.children[ind].y = getHourY(ind - hours - mins/60, 170);
+  }
+
+  seconds.text = secs < 10 ? '0' + secs : secs;
+}
+
+// Update the clock every tick event
+clock.ontick = (evt) => updateClock(evt);
